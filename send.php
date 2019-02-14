c391202b7fdd5bbc4554bdd3ef0a0ee5a1158a55
afterche <45674789+afterche@users.noreply.github.com>
2019-02-14 12:14:05 +0200

    upload mane file

---
+++ send.php
@@ -0,0 +1,103 @@
+<html>
+<head>
+<title>Форма заявки с сайта</title>
+</head>
+<body>
+
+<?php
+
+// ----------------------------конфигурация-------------------------- // 
+ 
+$adminemail="rfidinventory@protonmail.com";  // e-mail админа 
+ 
+ 
+$date=date("d.m.y"); // число.месяц.год 
+ 
+$time=date("H:i"); // часы:минуты:секунды 
+ 
+$backurl="index.html";  // На какую страничку переходит после отправки письма 
+ 
+//---------------------------------------------------------------------- // 
+ 
+  
+ 
+// Принимаем данные с формы 
+ 
+$name=$_POST['contact_name']; 
+ 
+$email=$_POST['contact_email']; 
+ 
+$msg=$_POST['contact_message']; 
+ 
+  
+ 
+// Проверяем валидность e-mail 
+ 
+if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", 
+strtolower($email))) 
+ 
+ { 
+ 
+  echo 
+"<center>Вернитесь <a 
+href='javascript:history.back(1)'><B>назад</B></a>. Вы 
+указали неверные данные!"; 
+ 
+  } 
+ 
+ else 
+ 
+ { 
+ 
+ 
+$msg=" 
+ 
+ 
+<p>Имя: $name</p> 
+ 
+ 
+<p>E-mail: $email</p> 
+ 
+ 
+<p>Сообщение: $msg</p> 
+ 
+ 
+"; 
+ 
+
+ 
+ // Отправляем письмо админу  
+ 
+mail($adminemail, "Ghbdtn", $msg); 
+ 
+  
+ 
+// Сохраняем в базу данных 
+ 
+$f = fopen("message.txt", "a+"); 
+ 
+fwrite($f," \n $date $time Сообщение от $name"); 
+ 
+fwrite($f,"\n $msg "); 
+ 
+fwrite($f,"\n ---------------"); 
+ 
+fclose($f); 
+ 
+  
+ 
+// Выводим сообщение пользователю 
+
+ 
+print "<script language='Javascript'><!-- 
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); 
+//--></script> 
+ 
+<p> Спасибо, что окликнулись. Мы свяжемся с Вами в ближайшее время. Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
+exit; 
+ 
+ } 
+ 
+?>
+</body>
+</html>
